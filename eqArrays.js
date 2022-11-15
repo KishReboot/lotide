63b977ea8adc8b33d1cafe858fb185095eca65ad
KishReboot <russellsalopek@pm.me>
2022-11-15 15:45:40 -0500

    Created a function that compares the length of an array, and compares the values within both arrays.

---
+++ eqArrays.js
@@ -0,0 +1,46 @@
+const assertEqual = (actual, expected) => {
+
+  actual !== expected
+    ? console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`)
+    : console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
+
+};
+
+
+/*
+ * Similar to assertEqual
+ * Comparing the length of two arrays to see if the values match (if statement)
+ * Then checking if the elements[i] match in the array
+ */
+const eqArrays = (arr1, arr2) => {
+
+  if (arr1.length !== arr2.length) {
+
+    return false;
+
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+
+    if(arr1[i] !== arr2[i]) 
+
+    return false;
+
+  }
+
+  return true;
+
+};
+
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
