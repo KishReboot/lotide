0453ecbd8850dc180d3c1664b4a6ec01e38114ac
KishReboot <russellsalopek@pm.me>
2022-11-16 09:56:08 -0500

    A little refactor

--- eqArrays.js
+++ eqArrays.js
@@ -16,17 +16,9 @@ const eqArrays = (arr1, arr2) => {
 
   for (let i = 0; i < arr1.length; i++) {
 
-    if (arr1.length !== arr2.length) {
+    if (arr1.length !== arr2.length) return false;
 
-      return false;
-
-    }
-
-    if (arr1[i] !== arr2[i]) {
-
-      return false;
-
-    }
+    if (arr1[i] !== arr2[i])  return false;
 
   }
   
