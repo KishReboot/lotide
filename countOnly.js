57eabce756441e8a4c9daaf46a8a74d0bee65054
KishReboot <russellsalopek@pm.me>
2022-11-16 12:03:45 -0500

    Added code to loop through, count all items, and then modified to count specific items

--- countOnly.js
+++ countOnly.js
@@ -18,18 +18,21 @@ const countOnly = function(allItems, itemsToCount) {
     // increment the counter for each item:
     // set a property with that string key to:
     // the value that was already there (or zero if nothing there) with 1 added
-    //
-    if (results[item]) {
-      
-      results[item] += 1;
     
-    } else {
-      
-      results[item] = 1;
-    
+    if (itemsToCount[item]) {
+
+      if (results[item]) {
+
+        results[item] += 1;
+
+      } else {
+
+        results[item] = 1;
+
+      }
+
     }
 
-    console.log(item);
   }
 
   return results;
