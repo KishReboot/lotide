d0e6221c595ed3cf8b85ac1ec27159f4bfb90a57
KishReboot <russellsalopek@pm.me>
2022-11-21 11:34:02 -0500

    Fixed minor issue, added same functionality to tail.js

--- head.js
+++ head.js
@@ -6,19 +6,5 @@ const head = (arr) => {
   if (arr === []) return undefined;
 };
 
+module.exports = head;
 
-/* You should always be thinking about other scenarios to consider.
- * Here are some such examples and how they should be handled:
- * An array with only one element should still yield that one element as its
- * head
- * An empty array should yield undefined as its head
- */
-
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([5,6,7]), 6);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Lighthouse");
-assertEqual(head([]), undefined);
-assertEqual(head([1, 2]), undefined);
-assertEqual(head([1]), 1);
