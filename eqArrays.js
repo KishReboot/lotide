3f9ee9807bd4b0360b21baccdcccc576eba82028
KishReboot <russellsalopek@pm.me>
2022-11-21 11:38:05 -0500

    Added functionality with require + export, so that we can run tests without having the function pasted multiple times throughout.

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,5 @@
-const assertEqual = (actual, expected) => {
+const assertEqual = require('./assertEqual');
 
-  actual !== expected
-    ? console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`)
-    : console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
-
-};
-
-
-/*
- * Similar to assertEqual
- * Comparing the length of two arrays to see if the values match (if statement)
- * Then checking if the elements[i] match in the array
- */
 const eqArrays = (arr1, arr2) => {
 
   for (let i = 0; i < arr1.length; i++) {
@@ -27,14 +15,4 @@ const eqArrays = (arr1, arr2) => {
 };
 
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
