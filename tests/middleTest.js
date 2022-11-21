17db834b4e94532a2c69d64162fbf294aa1a44aa
KishReboot <russellsalopek@pm.me>
2022-11-21 12:35:18 -0500

    Added mocha and chai functionality, using describe() and it() for tests

--- tests/middleTest.js
+++ tests/middleTest.js
@@ -1,18 +1,35 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+const assert = require('chai').assert;
+const middle   = require('../middle');
 
-// TEST CODE 
-// For arrays with one or two elements, there is no middle. Return an empty
-// array.
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
- 
-// For arrays with odd number of elements, an array containing a single middle
-// element should be returned.
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
+
+describe("#tail", () => {
+  
+  it("returns an empty array when there are only one or two elements, '[1] or [1, 2]", () => {
+    
+    assert.deepEqual(middle([1]), []);
+    assert.deepEqual(middle([1, 2]), []);
+
+  });
+
+  it("returns an array with 1 element when there is an odd number of elements, '[1, 2, 3] or [1, 2, 3, 4, 5]", () => {
+    
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+
+  });
+
+  it("returns an array with 2 elements when there is an even number of elements, '[1, 2, 3] or [1, 2, 3, 4, 5]", () => {
+    
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+  });
+
+});
+
+
   
 // For arrays with an even number of elements, an array containing the two
 // elements in the middle should be returned
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+//assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+//assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
--- tests/tailTest.js
+++ tests/tailTest.js
@@ -1,19 +1,12 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
+describe("#tail", () => {
+  
+  it("returns 'Lighthouse Labs' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", 'Labs']);
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+  });
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case for 1 element = empty
-const oneEle = tail(["Onepunch"]);
-assertEqual(oneEle.length, 1);
-assertEqual(oneEle[0]);
+});
