08fb5d9f226f82a4a0588f83c045cbeb9c1b0569
KishReboot <russellsalopek@pm.me>
2022-11-21 11:34:39 -0500

    Added functionality with require to ensure test files run.

---
+++ tests/headTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([5,6,7]), 6);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Lighthouse");
+assertEqual(head([]), undefined);
+assertEqual(head([1, 2]), undefined);
+assertEqual(head([1]), 1);
