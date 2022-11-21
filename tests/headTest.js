9836af8a0b4fdfc9dfdf5a0d59defac737e60d1b
KishReboot <russellsalopek@pm.me>
2022-11-21 12:19:26 -0500

    strictEqual to deepEqual

--- tests/headTest.js
+++ tests/headTest.js
@@ -17,19 +17,19 @@ describe("#head", () => {
 
   it("returns '5' for ['5', '6', '7']", () => {
 
-    assert.strictEqual(head([5, 6, 7]), 5);
+    assert.deepEqual(head([5, 6, 7]), 5);
   
   });
 
   it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
 
-    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+    assert.deepEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
   
   });
 
   it("returns '1' for [1]", () => {
 
-    assert.strictEqual(head([1]), 1);
+    assert.deepEqual(head([1]), 1);
   
   });
 
